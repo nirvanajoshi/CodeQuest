da83dba8e7864f5d8199e4b10b65d1e279b6a20c
nirvanajoshi <damodarj36@gmail.com>
2026-09-16 22:22:08 +0545

    courese app development

--- codequest/urls.py
+++ codequest/urls.py
@@ -23,6 +23,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('courses/', include('courses.urls')),
     path('challenges/', include('challenges.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.db.models import Prefetch
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from challenges.models import Challenge
+
+from .models import Course
+
+
+def course_list(request):
+    courses = Course.objects.filter(is_published=True).select_related("instructor")
+    return render(request, "courses/course_list.html", {"courses": courses})
+
+
+def course_detail(request, slug):
+    course = get_object_or_404(Course, slug=slug, is_published=True)
+    published_challenges = Challenge.objects.filter(is_published=True)
+    lessons = course.lessons.prefetch_related(
+        Prefetch("challenges", queryset=published_challenges)
+    )
+    return render(
+        request, "courses/course_detail.html", {"course": course, "lessons": lessons}
+    )
